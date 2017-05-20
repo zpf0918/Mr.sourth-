b9fff968070706ed353bcf513b2e02da8de932e6
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2017-05-20 11:15:57 +0800

    add model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170520002748) do
-
+ActiveRecord::Schema.define(version: 20170520021842) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
