a41ac3024c9558bdfcc46101c8754908261885f2
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2017-05-30 17:56:36 +0800

    alter config/initializers/rucaptcha.rb.temp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,13 +98,5 @@ Rails.application.configure do
     password: ENV["SEND_CLOUD_USER_KEY"]
     }
 
-  config.cache_store = :dalli_store,
-                   (ENV["MEMCACHIER_SERVERS"] || "").split(","),
-                   {:username => ENV["MEMCACHIER_USERNAME"],
-                    :password => ENV["MEMCACHIER_PASSWORD"],
-                    :failover => true,
-                    :socket_timeout => 1.5,
-                    :socket_failure_delay => 0.2,
-                    :down_retry_delay => 60
-                   }
+
 end
