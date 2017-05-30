113970bbc89b3cf243e84718b1ca4b01660a47a4
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2017-05-30 17:11:29 +0800

    add db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170527065921) do
+ActiveRecord::Schema.define(version: 20170530083201) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -82,6 +82,8 @@ ActiveRecord::Schema.define(version: 20170527065921) do
     t.integer  "position"
     t.integer  "category_id"
     t.boolean  "is_hidden",   default: false
+    t.string   "friendly_id"
+    t.index ["friendly_id"], name: "index_products_on_friendly_id", unique: true
   end
 
   create_table "reviews", force: :cascade do |t|
