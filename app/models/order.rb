6967e1137de909ab305ab901072bc0a5f851cfd0
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2017-06-03 14:06:26 +0800

    add

--- app/models/order.rb
+++ app/models/order.rb
@@ -27,10 +27,10 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :product_lists
 
-  validates :billing_name, presence: true
-  validates :billing_address, presence: true
-  validates :shipping_name, presence: true
-  validates :shipping_address, presence: true
+  # validates :billing_name, presence: true
+  # validates :billing_address, presence: true
+  # validates :shipping_name, presence: true
+  # validates :shipping_address, presence: true
 
   def set_payment_with!(method)
     self.update_columns(payment_method: method)
