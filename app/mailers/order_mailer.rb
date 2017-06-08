b8509e03bd6ca4938e8b7c9b16b8e211b5b0800a
jonathan <jonathan@kafeidebaideMacBook-Pro.local>
2017-06-08 16:14:06 +0800

    alter mailer content

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -4,7 +4,7 @@ class OrderMailer < ApplicationMailer
     @user        = order.user
     @product_lists = @order.product_lists
 
-    mail(to: @user.email , subject: "[JDstore] 感谢您完成本次的下单，以下是您这次购物明细 #{order.token}")
+    mail(to: @user.email , subject: "[南山先生] 感谢您完成本次的下单，以下是您这次购物明细 #{order.token}")
   end
 
   def apply_cancel(order)
@@ -12,7 +12,7 @@ class OrderMailer < ApplicationMailer
     @user = order.user
     @product_lists = @order.product_lists
 
-    mail(to: "1243281264@qq.com" ,subject: "[JD store] 用户#{order.user.email}申请取消订单 #{order.token}")
+    mail(to: "1243281264@qq.com" ,subject: "[南山先生] 用户#{order.user.email}申请取消订单 #{order.token}")
   end
 
   def notify_ship(order)
@@ -20,7 +20,7 @@ class OrderMailer < ApplicationMailer
     @user = order.user
     @product_lists = @order.product_lists
 
-    mail(to: @user.email, subject: "[JD store] 您的订单 #{order.token}已发货")
+    mail(to: @user.email, subject: "[南山先生] 您的订单 #{order.token}已发货")
   end
 
   def notify_cancel(order)
@@ -28,6 +28,6 @@ class OrderMailer < ApplicationMailer
     @user = order.user
     @product_lists = order.product_lists
 
-    mail(to: @user.email, subject: "[JD store] 您的订单 #{order.token}已取消}")
+    mail(to: @user.email, subject: "[南山先生] 您的订单 #{order.token}已取消}")
   end
 end
